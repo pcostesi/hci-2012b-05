4dec0bda621d8a491f7bc62e653670fa5f734586
Pablo Alejandro Costesich <rhfixer@gmail.com>
2012-10-01 16:33:10 -0300

    Base skeleton for the site

---
+++ TPE1/src/static/js/main.js
@@ -0,0 +1,11 @@
+// Create the namespace
+MindTrips = window.MindTrips || {}
+
+markup = $('#_template-test').html().replace(/\s\s+/g, '');
+template = Mustache.compile(markup);
+var text = "";
+for (var i = 0; i < 1000; i++) {
+	text += " lorem ipsum ";
+}
+$("#view").html(template({world:text}));
+$("#dates").datepicker();
