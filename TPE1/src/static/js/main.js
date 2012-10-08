780b5ded45f3d0762db4f9c48a5fb25d23d50cf6
Matias Domingues <matiasdomingues@gmail.com>
2012-10-08 17:56:45 -0300

    selector template

--- TPE1/src/static/js/main.js
+++ TPE1/src/static/js/main.js
@@ -160,18 +160,6 @@ function initialize() {
     var map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
 }
 
-function codeAddress() {
-   	var address = document.getElementById("address").value;
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      	if (status == google.maps.GeocoderStatus.OK) {
-        	map.setCenter(results[0].geometry.location);
-        	var marker = new google.maps.Marker({ map: map, position: results[0].geometry.location});
-      	} else {
-        	alert("Geocode was not successful for the following reason: " + status);
-      	}
-    });
-}
-
 function addHiddenToReturn(){
     $("#return-dates").addClass("hidden");
 }
