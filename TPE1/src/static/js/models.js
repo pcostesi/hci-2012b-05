6940ce11a1462b6565d45c6bc9083adf1c9f7fe1
Pablo Alejandro Costesich <rhfixer@gmail.com>
2012-10-16 19:59:53 -0300

    BIG refactory

---
+++ TPE1/src/static/js/models.js
@@ -0,0 +1,17 @@
+
+// Create the namespace
+MindTrips = window.MindTrips || {}
+var MindTrips = MindTrips;
+
+
+
+MindTrips.BaseModel = Backbone.Model.extend({});
+
+MindTrips.BaseCollection = Backbone.Collection.extend({});
+
+
+MindTrips.Airline = MindTrips.BaseModel.extend({
+    defaults: {
+
+    },
+});
