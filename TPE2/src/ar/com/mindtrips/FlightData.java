2a7e62c60a23cfa96a90c3e2de0916fd423e47da
Matias Domingues <matiasdomingues@gmail.com>
2012-11-07 23:38:57 -0300

    fragments

---
+++ TPE2/src/ar/com/mindtrips/FlightData.java
@@ -0,0 +1,16 @@
+package ar.com.mindtrips;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FlightData extends Fragment {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+            Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_flight_data, container, false);
+        }
+}
--- TPE2/src/ar/com/mindtrips/MainActivity.java
+++ TPE2/src/ar/com/mindtrips/MainActivity.java
@@ -3,18 +3,26 @@ package ar.com.mindtrips;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.ProgressBar;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        FlightSelector selector = new FlightSelector();
+        FlightData activity = new FlightData();
+        getSupportFragmentManager().beginTransaction().add(R.id.flight_data_container, activity).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, selector).commit();
+        ProgressBar progress = (ProgressBar) findViewById(R.id.loading);
+        progress.setVisibility(View.INVISIBLE);
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,3 +48,4 @@ public class MainActivity extends Activity {
         }
     }
 }
+
