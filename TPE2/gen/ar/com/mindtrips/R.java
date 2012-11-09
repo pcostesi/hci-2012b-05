cd8c2551f9dd7615c9b19765e77fcf8f0f1a63c1
Pablo Alejandro Costesich <rhfixer@gmail.com>
2012-11-09 20:22:24 -0300

    last minute fix

--- TPE2/gen/ar/com/mindtrips/R.java
+++ TPE2/gen/ar/com/mindtrips/R.java
@@ -11,9 +11,9 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int padding_large=0x7f040002;
-        public static final int padding_medium=0x7f040001;
-        public static final int padding_small=0x7f040000;
+        public static final int padding_large=0x7f060002;
+        public static final int padding_medium=0x7f060001;
+        public static final int padding_small=0x7f060000;
     }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
@@ -38,15 +38,15 @@ public final class R {
         public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int airport_or_city=0x7f050005;
-        public static final int app_name=0x7f050000;
-        public static final int arrival=0x7f050006;
-        public static final int departure=0x7f050004;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
-        public static final int title_activity_main=0x7f050003;
+        public static final int airport_or_city=0x7f040005;
+        public static final int app_name=0x7f040000;
+        public static final int arrival=0x7f040006;
+        public static final int departure=0x7f040004;
+        public static final int hello_world=0x7f040001;
+        public static final int menu_settings=0x7f040002;
+        public static final int title_activity_main=0x7f040003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f050000;
     }
 }
