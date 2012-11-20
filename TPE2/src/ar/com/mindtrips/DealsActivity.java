cc60b426182840d0905992a046c0d709f4f266af
Pablo Alejandro Costesich <rhfixer@gmail.com>
2012-11-19 23:12:56 -0300

    emergency commit

--- TPE2/src/ar/com/mindtrips/DealsActivity.java
+++ TPE2/src/ar/com/mindtrips/DealsActivity.java
@@ -1,24 +1,48 @@
 package ar.com.mindtrips;
 
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
+import ar.edu.itba.hci2012.api.RequestReceiver;
 
 public class DealsActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_deals);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_deals);
 
-	      System.out.println("YAY FUCKERS");
-	    // Get the intent, verify the action and get the query
-	    Intent intent = getIntent();
-	    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-	      String query = intent.getStringExtra(SearchManager.QUERY);
-	      //doMySearch(query);
+		// Get the intent, verify the action and get the query
+		Intent intent = getIntent();
+		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			// doMySearch(query);
 
-	      System.out.println("QUERYING HELL");
-	    }
+			RequestReceiver receiver = new RequestReceiver() {
+
+				@Override
+				public void onSuccess(JSONObject json) {
+					ListView mlistView = (ListView) findViewById(R.id.dealsList);
+
+				}
+			};
+		}
+
+		/*
+		 * 
+		 * ListView mlistView = (ListView) findViewById(R.id.dealsList);
+		 * mlistView.setAdapter(new ArrayAdapter<String>(this,
+		 * android.R.layout.simple_list_item_1, new String[] {"Game", "Help",
+		 * "Home site"}));
+		 * 
+		 * mlistView.setOnItemClickListener(new OnItemClickListener() { public
+		 * void onItemClick(AdapterView<?> parent, View view, int position, long
+		 * id) { // When clicked, show a toast with the TextView text Game,
+		 * Help, Home Toast.makeText(getApplicationContext(), ((TextView)
+		 * view).getText(), Toast.LENGTH_SHORT).show(); } });
+		 */
 	}
 }
